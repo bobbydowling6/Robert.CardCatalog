cc87f748af8fba6b368dee6c5823d510253e0413
bobbydowling6 <bobbydowling6@gmail.com>
2017-09-28 14:54:10 -0500

    Added some fields

--- Robert.CardCatalog/CardCatalog.cs
+++ Robert.CardCatalog/CardCatalog.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace Robert.CardCatalog
 {
     public class CardCatalog
     {
-        public CardCatalog()
+        public CardCatalog(string fileName)
         {
+            _fileName = fileName;
         }
+
+        private string _fileName;
+
+        private List<Book> books;
     }
 }
