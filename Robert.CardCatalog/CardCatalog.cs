c851691960dc73af13ce62b26fcb10ce49c84aac
Joe Johnson <joe.wendell.johnson@gmail.com>
2017-09-28 14:55:57 -0500

    Added other method stubs

--- Robert.CardCatalog/CardCatalog.cs
+++ Robert.CardCatalog/CardCatalog.cs
@@ -8,10 +8,29 @@ namespace Robert.CardCatalog
         public CardCatalog(string fileName)
         {
             _fileName = fileName;
+            books = new List<Book>();
         }
 
         private string _fileName;
 
         private List<Book> books;
+
+        //TODO: Correct return type and parameters
+        public void ListBooks()
+        {
+
+        }
+
+        //TODO: Correct return type and parameters
+        public void AddBook()
+        {
+
+        }
+
+        //TODO: Correct return type and parameters
+        public void Save()
+        {
+
+        }
     }
 }
